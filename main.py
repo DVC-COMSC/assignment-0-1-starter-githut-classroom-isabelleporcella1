ec5b6555fbde2281bda960f90e60a1f30a69bced
isabelleporcella1 <isabelle.porcella@outlook.com>
2023-06-17 15:46:42 -0700

    Create main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 def main():
-
-    ##############################
-    # make your code below
-    # print('Hello World')
-    ##############################
+    print('Hello World')
+    print('Python Programming')
 
 
 if __name__ == '__main__':
